ff7a1d544f6aa10f773f00e8c5350c1977a6665b
张文文 <18622076193@163.com>
2018-06-05 21:34:25 +0800

    评论列表 我的图书列表 booklist后端修改

--- src/pages/comments/main.js
+++ src/pages/comments/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Comment from './Comment'
 
-Vue.config.productionTip = false
 const app = new Vue(Comment)
 app.$mount()
+
+export default{
+  config:{
+    navigationBarTitleText:'评论列表',
+    enablePullDownRefresh:true
+  }
+  
+}
